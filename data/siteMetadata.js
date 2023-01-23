e92aadbb67f662251842673b7fa2dd968ff8be7b
pelatge <pelat.g29@gmail.com>
2023-01-23 16:56:52 +0700

    add mukharomdev.jpg

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteUrl: 'https://mukharom.vercel.app',
   siteRepo: 'https://github.com/Mukharomdev/blog-nextjs-vercel',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/mukharomdev.jpg',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
   github: 'https://github.com',
